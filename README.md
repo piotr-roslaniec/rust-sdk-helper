3371c0d0b876475e321e1ffbbf58086b27a63dd6
Piotr Rosłaniec <p.roslaniec@gmail.com>
2019-06-04 13:00:11 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# rust-sdk container helper
+
+## Usage
+
+```bash
+./build.bash
+cp rust-sdk.bash ~/scripts/rust-sdk # or other preffered location in $PATH
+cd /path/to/my/rust/sgx/project
+rust-sdk --help
+```
---
+++ rust-sdk.bash
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+IMAGE=rust-sdk
+SRC=$(pwd)
+CONTAINER=$IMAGE-$(basename $SRC)
+
+usage() {
+    cat <<EOF
+usage: $0 options
+
+Set up Docker container with baidu/rust-sgx-sdk.
+
+OPTIONS:
+   -h, --help    Show this message
+   -r, --run     Create or recreate container
+   -a, --attach  Attach shell to existing container
+   -k, --kill    Kill container if exists
+EOF
+}
+
+run() {
+    if ls /dev/isgx >/dev/null &>/dev/null; then
+        echo "SGX Driver installed"
+    else
+        echo "SGX Driver NOT installed"
+        exit 1
+    fi
+
+    kill
+
+    docker run -d -it -v $SRC:/root/src:rw --device /dev/isgx -p 5222:5222 --name $CONTAINER $IMAGE
+
+    attach
+}
+
+attach() {
+    docker exec -it $CONTAINER bash
+}
+
+kill() {
+    docker rm -f $CONTAINER &>/dev/null && echo 'Removed old container'
+}
+
+while test $# -gt 0; do
+    case "$1" in
+    -h | --help)
+        usage
+        exit 1
+        ;;
+    -r | --run)
+        shift
+        run
+        exit 0
+        ;;
+    -a | --attach)
+        shift
+        attach
+        exit 0
+        ;;
+    -k | --kill)
+        shift
+        kill
+        exit 0
+        ;;
+    ?)
+        usage
+        exit 1
+        ;;
+    esac
+done
